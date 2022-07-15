2cff74bae15c57b3d930de932ed7aab12c0202ee
Kellen Cataldo <kellen.cataldo@discordapp.com>
2022-07-15 21:35:23 +0000

    basic tests lol

---
+++ test_calc.py
@@ -0,0 +1,15 @@
+import pytest
+from dataclasses import dataclass
+from calc import calc
+
+
+@pytest.mark.parametrize("expression_input,expression_output,expect_throw", 
+    [
+        pytest.param("0", 0, False, id="basic_expression")
+    ]
+)
+def test_eval(expression_input: str, expression_output: int, expect_throw: bool):
+    try:
+        assert expression_output == calc(expression_input)
+    except:
+        assert expect_throw
